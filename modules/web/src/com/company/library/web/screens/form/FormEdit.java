6d094e6b637cb21923b6c4b07319ffc3ad634916
Colleen <Colleenbawn.514@gmail.com>
2020-07-20 03:31:29 +0400

    Second commit

--- modules/web/src/com/company/library/web/screens/form/FormEdit.java
+++ modules/web/src/com/company/library/web/screens/form/FormEdit.java
@@ -1,18 +1,21 @@
 package com.company.library.web.screens.form;
 
 import com.company.library.entity.Book;
+import com.company.library.entity.Reader;
 import com.haulmont.cuba.core.global.DataManager;
 import com.haulmont.cuba.core.global.Metadata;
 import com.haulmont.cuba.core.global.MetadataTools;
 import com.haulmont.cuba.gui.Notifications;
 import com.haulmont.cuba.gui.components.Button;
 import com.haulmont.cuba.gui.components.PickerField;
+import com.haulmont.cuba.gui.components.ValidationException;
 import com.haulmont.cuba.gui.model.InstanceContainer;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.library.entity.Form;
 
 import javax.inject.Inject;
 import java.awt.*;
+import java.util.Map;
 
 @UiController("library_Form.edit")
 @UiDescriptor("form-edit.xml")
@@ -52,6 +55,26 @@ public class FormEdit extends StandardEditor<Form> {
     private Button saveButton;
     @Inject
     private PickerField<Book> bookField;
+    @Inject
+    private PickerField<Reader> readerField;
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+        bookField.addValidator(value -> {
+            Reader reader = readerField.getValue();
+            if(value !=null && reader!=null){
+                Boolean alreadyExist = isExistBook(value, reader.getFullName());
+                saveButton.setEnabled(!alreadyExist);
+
+                if(alreadyExist){
+                    notifications.create(Notifications.NotificationType.TRAY)
+                            .withCaption("There is such a book on hand")
+                            .show();
+                    throw new ValidationException("You cannot select admin user");
+                }
+            }
+        });
+    }
 
     @Subscribe(id = "formDc", target = Target.DATA_CONTAINER)
     protected void onOrderDcItemPropertyChange(InstanceContainer.ItemPropertyChangeEvent<Form> event) {
@@ -62,14 +85,7 @@ public class FormEdit extends StandardEditor<Form> {
         if(book==null) {
             return;
         }
-        Boolean alreadyExist = isExistBook(book, event.getItem().getReader().getFullName());
-        saveButton.setEnabled(!alreadyExist);
-        bookField.setFrame(Frame.ERROR);
-        if(alreadyExist){
-            notifications.create(Notifications.NotificationType.TRAY)
-                    .withCaption("There is such a book on hand")
-                    .show();
-        }
+       bookField.validate();
     }
 
     private Boolean isExistBook(Book book, String reader){
